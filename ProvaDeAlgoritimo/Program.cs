9d54594dbcb218ea8da0f5f7c055617f0903d4ba
ivanmslima <81934424+ivanmslima@users.noreply.github.com>
2022-12-01 21:08:28 -0300

    Adicionar arquivos de projeto.

---
+++ ProvaDeAlgoritimo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace curso
+{
+    class Progam
+    {
+        static void Main(string[] args)
+        {
+            int cont = 1;
+            int maior = 0;
+
+            while(cont <= 10)
+            {
+                Console.WriteLine("Digite um número: ");
+                int n = int.Parse(Console.ReadLine());
+
+                if (n>maior)
+                {
+                    maior = n;
+                }
+                cont++;
+                
+            }
+            Console.WriteLine("O maior número é {0}", maior);
+
+
+        }
+    }
+}
